28c57ea1582dcaf99decb60ecc67661867775693
Sandip Patel <Sandipbhai.Patel@epay.ingenico.com>
2020-08-13 11:31:58 +0200

    Fixed jupiter test cases by removing jUnit4 stuffs.

--- src/test/java/com/avengers/hackathon/repository/CustomerRepositoryTest.java
+++ src/test/java/com/avengers/hackathon/repository/CustomerRepositoryTest.java
@@ -6,14 +6,11 @@ import org.junit.Assert;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Optional;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest(classes = ProductServiceApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class CustomerRepositoryTest {
--- src/test/java/com/avengers/hackathon/repository/ProductRepositoryTest.java
+++ src/test/java/com/avengers/hackathon/repository/ProductRepositoryTest.java
@@ -1,22 +1,17 @@
 package com.avengers.hackathon.repository;
 
 import com.avengers.hackathon.ProductServiceApplication;
-import com.avengers.hackathon.bean.Product;
-import com.avengers.hackathon.model.Customer;
 import com.avengers.hackathon.model.ProductEntity;
 import org.junit.Assert;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import java.math.BigDecimal;
 import java.util.Optional;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest(classes = ProductServiceApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class ProductRepositoryTest {
